d6a87be09e60ffbd041b2d1e2855a2d092733c7a
Rafael <rafael2005leal@gmail.com>
2025-11-03 22:43:19 -0300

    Add process status file reading to monitor_process
    The monitor_process function now opens and reads the /proc/[pid]/status file, printing its contents. Error handling for file opening has been added.

--- src/main.c
+++ src/main.c
@@ -6,4 +6,21 @@ void monitor_process(int pid){
     FILE *fp;
 
     sprintf(proc_path, "/proc/%d/status", pid);
+    fp = (fopen(proc_path, "r"));
+    if(fp == NULL){
+        perror("Erro ao abrir o arquivo /proc");
+        return;
+    }
+
+    char buffer[4096];
+    if (fgets(buffer, sizeof(buffer), fp) != NULL)
+    {
+        printf("\nConteúdo do /proc/%d/status: \n%s\n", pid, buffer);
+    }
+    
+    fclose(fp);
+}
+
+int main(int argc, char *argv[]){
+    
 }
