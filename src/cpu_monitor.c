e0a5674079d0186fb4801729b69d19550808c7ca
Rafael <rafael2005leal@gmail.com>
2025-11-08 12:20:16 -0300

    CPU Monitor completo
    Agora, cpu_monitor está completo, com todas as suas funções de switches, threads, system time e user time.

--- src/cpu_monitor.c
+++ src/cpu_monitor.c
@@ -3,6 +3,54 @@
 #include <string.h>
 #include "monitor.h"
 
+int metricas_switches(int pid, CpuMetrics *cpu){
+    char proc_path[256];
+    FILE *fp;
+
+    sprintf(proc_path, "/proc/%d/status", pid); 
+    fp = fopen(proc_path, "r");
+
+    if(fp == NULL){
+        perror("Erro ao abrir o processo");
+        return -1;
+    }
+
+    char buffer[4096];
+    char *pt;
+    long voluntary = 0;
+    long nonvoluntary = 0;
+
+    int valor_encontrado_voluntary = 0;
+    int valor_encontrado_nonvoluntary = 0;
+
+
+    while(fgets(buffer, sizeof(buffer), fp) != NULL){
+        if (valor_encontrado_voluntary == 0 && strstr(buffer, "voluntary_ctxt_switches") != NULL){
+            pt = strchr(buffer, ":");
+            if(pt != NULL){
+                voluntary = atol(pt + 1);
+                valor_encontrado_voluntary = 1;
+            }
+        }
+        if (valor_encontrado_nonvoluntary == 0 && strstr(buffer, "nonvoluntary_ctxt_switches") != NULL){
+            pt = strchr(buffer, ":");
+            if(pt != NULL){
+                nonvoluntary = atol(pt + 1);
+                valor_encontrado_nonvoluntary = 1;
+            }
+        }
+        if (valor_encontrado_voluntary == 1 && valor_encontrado_nonvoluntary == 1){
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    cpu -> switches = voluntary + nonvoluntary;
+
+    return 0;
+}
+
 int metricas_CPU(int pid, CpuMetrics *cpu){
     char proc_path[256];
     FILE *fp;
@@ -12,16 +60,15 @@ int metricas_CPU(int pid, CpuMetrics *cpu){
 
     if(fp == NULL){
         perror("Erro ao abrir o processo");
-        return;
+        return -1;
     }
 
     char buffer[4096];
-    fgets(buffer, sizeof(buffer), fp);
-    if (buffer == NULL)
+    if (fgets(buffer, sizeof(buffer), fp) == NULL)
     {
         printf("Erro ao ler dados do arquivo %s", proc_path);
         fclose(fp);
-        return;
+        return -1;
     }
 
     fclose(fp);
@@ -34,15 +81,18 @@ int metricas_CPU(int pid, CpuMetrics *cpu){
 
     token = strtok(buffer, " ");
 
-    while (token != NULL && contagem <= 15){
+    while (token != NULL && contagem <= 20){
         switch (contagem)
         {
         case 14:
             cpu -> user_time = atol(token);
+            break;
         case 15:
             cpu -> system_time = atol(token);
+            break;
         case 20:
             cpu -> threads = atol(token);
+            break;
         }
         token = strtok(NULL, " ");
         contagem++;
