eeae90d691ec05c264d09d17b6c5c45ea412d27e
HenriqueSanchesAndr <sanches.henrique@pucpr.edu.br>
2025-11-16 22:18:57 -0300

    correção2

--- src/cpu_monitor.c
+++ src/cpu_monitor.c
@@ -3,62 +3,69 @@
 #include <string.h>
 #include "monitor.h"
 
-int metricas_switches(int pid, CpuMetrics *cpu){
+//Coleta o número de switcher de um processo
+int metricas_switches(int pid, CpuMetrics* cpu) {
     char proc_path[256];
-    FILE *fp;
+    FILE* fp;
 
-    sprintf(proc_path, "/proc/%d/status", pid); 
+    //Monta o caminho para o arquivo /proc/[PID]/status
+    sprintf(proc_path, "/proc/%d/status", pid);
     fp = fopen(proc_path, "r");
 
-    if(fp == NULL){
+    //Erro caso não consiga abrir
+    if (fp == NULL) {
         perror("Erro ao abrir o processo");
         return -1;
     }
 
     char buffer[4096];
-    char *pt;
+    char* pt;
     long voluntary = 0;
     long nonvoluntary = 0;
 
     int valor_encontrado_voluntary = 0;
     int valor_encontrado_nonvoluntary = 0;
 
-
-    while(fgets(buffer, sizeof(buffer), fp) != NULL){
-        if (valor_encontrado_voluntary == 0 && strstr(buffer, "voluntary_ctxt_switches") != NULL){
+    //Procura pelas linhas mudança de contexto
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (valor_encontrado_voluntary == 0 && strstr(buffer, "voluntary_ctxt_switches") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
+            if (pt != NULL) {
                 voluntary = atol(pt + 1);
                 valor_encontrado_voluntary = 1;
             }
         }
-        if (valor_encontrado_nonvoluntary == 0 && strstr(buffer, "nonvoluntary_ctxt_switches") != NULL){
+        if (valor_encontrado_nonvoluntary == 0 && strstr(buffer, "nonvoluntary_ctxt_switches") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
-                nonvoluntary = atol(pt + 1);
+            if (pt != NULL) {
+                nonvoluntary = atol(pt + 1); //Converte a string para long
                 valor_encontrado_nonvoluntary = 1;
             }
         }
-        if (valor_encontrado_voluntary == 1 && valor_encontrado_nonvoluntary == 1){
+        //Para o loop se já encontrou os dois valores
+        if (valor_encontrado_voluntary == 1 && valor_encontrado_nonvoluntary == 1) {
             break;
         }
     }
 
     fclose(fp);
 
-    cpu -> switches = voluntary + nonvoluntary;
+    //Armazena o total de mudanças de contexto
+    cpu->switches = voluntary + nonvoluntary;
 
     return 0;
 }
 
-int metricas_CPU(int pid, CpuMetrics *cpu){
+//Coleta as métricas de CPU
+int metricas_CPU(int pid, CpuMetrics* cpu) {
     char proc_path[256];
-    FILE *fp;
+    FILE* fp;
 
+    //Monta caminho para o arquivo proc, pid e stat
     sprintf(proc_path, "/proc/%d/stat", pid);
     fp = fopen(proc_path, "r");
 
-    if(fp == NULL){
+    if (fp == NULL) {
         perror("Erro ao abrir o processo");
         return -1;
     }
@@ -73,25 +80,25 @@ int metricas_CPU(int pid, CpuMetrics *cpu){
 
     fclose(fp);
 
-    char *token; //Para percorrer o buffer
+    char* token; //Para percorrer o buffer
     int contagem = 1;
-    cpu -> user_time = 0;
-    cpu -> system_time = 0;
-    cpu -> threads = 0;
+    cpu->user_time = 0;
+    cpu->system_time = 0;
+    cpu->threads = 0;
 
     token = strtok(buffer, " ");
-
-    while (token != NULL && contagem <= 20){
+    //Pega os valores user_time, system_time e threads
+    while (token != NULL && contagem <= 20) {
         switch (contagem)
         {
         case 14:
-            cpu -> user_time = atol(token);
+            cpu->user_time = atol(token);
             break;
         case 15:
-            cpu -> system_time = atol(token);
+            cpu->system_time = atol(token);
             break;
         case 20:
-            cpu -> threads = atol(token);
+            cpu->threads = atol(token);
             break;
         }
         token = strtok(NULL, " ");
@@ -101,28 +108,29 @@ int metricas_CPU(int pid, CpuMetrics *cpu){
     return 0;
 }
 
+//Coleta o total de ticks do sistema
 long total_ticks_sistema() {
-    FILE *fp;
+    FILE* fp;
     fp = fopen("/proc/stat", "r");
 
-    if (fp == NULL){
+    if (fp == NULL) {
         perror("Erro ao abrir o processo");
         return -1;
     }
 
     char buffer[4096];
-    if(fgets(buffer, sizeof(buffer), fp) == NULL){
+    if (fgets(buffer, sizeof(buffer), fp) == NULL) {
         fclose(fp);
         return -1;
     }
     fclose(fp);
 
-    char *token = strtok(buffer, " ");
+    char* token = strtok(buffer, " ");
     long total_ticks = 0;
     int i = 0;
-
-    while((token = strtok(NULL, " ")) != NULL){
-        if (i >= 7){
+    //Pega os valores de ticks somados
+    while ((token = strtok(NULL, " ")) != NULL) {
+        if (i >= 7) {
             break;
         }
         total_ticks = total_ticks + atol(token);
--- src/io_monitor.c
+++ src/io_monitor.c
@@ -1,88 +1,95 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "monitor.h"
 
-int metricas_IO(int pid, IoMetrics *io){
+//Coleta metricas das I/O do processo
+int metricas_IO(int pid, IoMetrics* io) {
     char proc_path[256];
-    FILE *fp;
+    FILE* fp;
 
+    //Monta o caminho proc,PID, io
     sprintf(proc_path, "/proc/%d/io", pid);
-    fp = fopen(proc_path , "r");
+    fp = fopen(proc_path, "r");
 
-    if(fp == NULL){
+    //Caso nao tenha processo ou permissão, erro
+    if (fp == NULL) {
         perror("Erro ao abrir o processo");
         return -1;
     }
 
+    //Criar buffer para cada linha, pesquisar
     char buffer[4096];
-    char *pt;
+    char* pt;
     int vlr_rb = 0, vlr_wb = 0, vlr_diskop_l = 0, vlr_diskop_e = 0, vlr_sys_l = 0, vlr_sys_e = 0;
-    io -> read_bytes = 0;
-    io -> write_bytes = 0;
+    io->read_bytes = 0;
+    io->write_bytes = 0;
     long diskop_leitura = 0;
     long diskop_escrita = 0;
     long syscall_leitura = 0;
     long syscall_escrita = 0;
 
-    while(fgets(buffer, sizeof(buffer), fp) != NULL){
-        // read_bytes
-        if(vlr_rb == 0 && strstr(buffer, "rchar") != NULL){
+    //Procura métricas linha por linha
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        // captura rcha 
+        if (vlr_rb == 0 && strstr(buffer, "rchar") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
-                io -> read_bytes = atol(pt + 1);
+            if (pt != NULL) {
+                io->read_bytes = atol(pt + 1);
                 vlr_rb = 1;
             }
         }
-        // write_bytes
-        if(vlr_wb == 0 && strstr(buffer, "wchar") != NULL){
+        // captura wchar
+        if (vlr_wb == 0 && strstr(buffer, "wchar") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
-                io -> write_bytes = atol(pt + 1);
+            if (pt != NULL) {
+                io->write_bytes = atol(pt + 1);
                 vlr_wb = 1;
             }
         }
-        // disk_op_leitura
-        if(vlr_diskop_l == 0 && strstr(buffer, "read_bytes") != NULL){
+        // captura read_bytes(Bytes lidos do disco)
+        if (vlr_diskop_l == 0 && strstr(buffer, "read_bytes") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
+            if (pt != NULL) {
                 diskop_leitura = atol(pt + 1);
                 vlr_diskop_l = 1;
             }
         }
-        // disk_op_escrita
-        if(vlr_diskop_e == 0 && strstr(buffer, "write_bytes") != NULL){
+        // captura writhe_bytes
+        if (vlr_diskop_e == 0 && strstr(buffer, "write_bytes") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
+            if (pt != NULL) {
                 diskop_escrita = atol(pt + 1);
                 vlr_diskop_e = 1;
             }
         }
-        // syscall_leitura
-        if(vlr_sys_l == 0 && strstr(buffer, "syscr") != NULL){
+        // captura o syscr
+        if (vlr_sys_l == 0 && strstr(buffer, "syscr") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
+            if (pt != NULL) {
                 syscall_leitura = atol(pt + 1);
                 vlr_sys_l = 1;
             }
         }
         // syscall_escrita
-        if(vlr_sys_e == 0 && strstr(buffer, "syscw") != NULL){
+        if (vlr_sys_e == 0 && strstr(buffer, "syscw") != NULL) {
             pt = strchr(buffer, ':');
-            if(pt != NULL){
+            if (pt != NULL) {
                 syscall_escrita = atol(pt + 1);
                 vlr_sys_e = 1;
             }
         }
-        if(vlr_diskop_e == 1 && vlr_diskop_l == 1 && vlr_rb == 1 && vlr_sys_e == 1 && vlr_sys_l == 1 && vlr_wb == 1){
+        //Sai do loop caso todas metricas capturadas
+        if (vlr_diskop_e == 1 && vlr_diskop_l == 1 && vlr_rb == 1 && vlr_sys_e == 1 && vlr_sys_l == 1 && vlr_wb == 1) {
             break;
         }
     }
 
     fclose(fp);
-
-    io -> disk_op = diskop_escrita + diskop_leitura;
-    io -> syscall = syscall_escrita + syscall_leitura;
+    //calcula os totais de operações no disco e syscalls
+    io->disk_op = diskop_escrita + diskop_leitura;
+    io->syscall = syscall_escrita + syscall_leitura;
 
     return 0;
 }
